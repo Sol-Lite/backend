fa53d216cf11ca9c99123a5c31a227dbe72aa391
combe4259 <han97901@gmail.com>
2026-03-16 01:10:29 +0900

    init: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/sollite/SolLiteApplication.java
@@ -0,0 +1,12 @@
+package com.sollite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolLiteApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sollite/SolLiteApplicationTest.java
@@ -0,0 +1,12 @@
+package com.sollite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolLiteApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
