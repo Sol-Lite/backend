1f388f1102a76d1714db64afd15d938cc9bb90ab
combe4259 <han97901@gmail.com>
2026-03-18 14:16:57 +0900

    chore(config): V3 마이그레이션 파일 복구

--- src/main/resources/db/migration/V3__drop_marketing_agreed.sql
+++ src/main/resources/db/migration/V3__drop_marketing_agreed.sql
@@ -1,5 +1,12 @@
 -- users 테이블에서 불필요한 컬럼 제거
 -- 동의 이력을 별도 테이블로 관리
+-- users 테이블에서 불필요한 컬럼 제거
+ALTER TABLE users DROP COLUMN marketing_agreed_yn;
+ALTER TABLE users DROP COLUMN service_terms_agreed_yn;
+ALTER TABLE users DROP COLUMN privacy_terms_agreed_yn;
+ALTER TABLE users DROP COLUMN terms_agreed_at;
+ALTER TABLE users DROP COLUMN language;
+
 
 CREATE TABLE user_consents (
     user_id                  NUMBER(19,0)  PRIMARY KEY,
