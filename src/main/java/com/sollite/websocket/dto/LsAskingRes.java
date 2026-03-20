9362dc51a9e0d42a055a54c36963b60607ddb7d4
kminnaaa <bexemen@gmail.com>
2026-03-20 17:36:11 +0900

    fix(websocket): UH1 응답 필드명에 맞게 LsAskingRes 수정
    - unt_totofferrem, unt_totbidrem 필드 매핑 추가
    - unt_offerrem1~10, unt_bidrem1~10 필드 매핑 추가
    - @JsonIgnoreProperties 추가로 불필요한 필드 무시

--- src/main/java/com/sollite/websocket/dto/LsAskingRes.java
+++ src/main/java/com/sollite/websocket/dto/LsAskingRes.java
@@ -1,60 +1,82 @@
 package com.sollite.websocket.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record LsAskingRes(
         LsAskingHeader header,
         LsAskingBody body
 ) {
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record LsAskingHeader(
             String tr_cd
     ) {}
 
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public record LsAskingBody(
             String hotime,
             String shcode,
             String volume,
-            String totofferrem,
-            String totbidrem,
+            @JsonProperty("unt_totofferrem") String untTotofferrem,
+            @JsonProperty("unt_totbidrem")   String untTotbidrem,
             String offerho1,  String offerho2,  String offerho3,  String offerho4,  String offerho5,
             String offerho6,  String offerho7,  String offerho8,  String offerho9,  String offerho10,
-            String offerrem1, String offerrem2, String offerrem3, String offerrem4, String offerrem5,
-            String offerrem6, String offerrem7, String offerrem8, String offerrem9, String offerrem10,
+            @JsonProperty("unt_offerrem1")  String untOfferrem1,
+            @JsonProperty("unt_offerrem2")  String untOfferrem2,
+            @JsonProperty("unt_offerrem3")  String untOfferrem3,
+            @JsonProperty("unt_offerrem4")  String untOfferrem4,
+            @JsonProperty("unt_offerrem5")  String untOfferrem5,
+            @JsonProperty("unt_offerrem6")  String untOfferrem6,
+            @JsonProperty("unt_offerrem7")  String untOfferrem7,
+            @JsonProperty("unt_offerrem8")  String untOfferrem8,
+            @JsonProperty("unt_offerrem9")  String untOfferrem9,
+            @JsonProperty("unt_offerrem10") String untOfferrem10,
             String bidho1,  String bidho2,  String bidho3,  String bidho4,  String bidho5,
             String bidho6,  String bidho7,  String bidho8,  String bidho9,  String bidho10,
-            String bidrem1, String bidrem2, String bidrem3, String bidrem4, String bidrem5,
-            String bidrem6, String bidrem7, String bidrem8, String bidrem9, String bidrem10
+            @JsonProperty("unt_bidrem1")  String untBidrem1,
+            @JsonProperty("unt_bidrem2")  String untBidrem2,
+            @JsonProperty("unt_bidrem3")  String untBidrem3,
+            @JsonProperty("unt_bidrem4")  String untBidrem4,
+            @JsonProperty("unt_bidrem5")  String untBidrem5,
+            @JsonProperty("unt_bidrem6")  String untBidrem6,
+            @JsonProperty("unt_bidrem7")  String untBidrem7,
+            @JsonProperty("unt_bidrem8")  String untBidrem8,
+            @JsonProperty("unt_bidrem9")  String untBidrem9,
+            @JsonProperty("unt_bidrem10") String untBidrem10
     ) {
-        public long parsedTotOfferRem() { return parse(totofferrem()); }
-        public long parsedTotBidRem()   { return parse(totbidrem()); }
+        public long parsedTotOfferRem() { return parse(untTotofferrem()); }
+        public long parsedTotBidRem()   { return parse(untTotbidrem()); }
 
         public List<AskingResponse.OrderEntry> toAsks() {
             return List.of(
-                    new AskingResponse.OrderEntry(parse(offerho1()),  parse(offerrem1())),
-                    new AskingResponse.OrderEntry(parse(offerho2()),  parse(offerrem2())),
-                    new AskingResponse.OrderEntry(parse(offerho3()),  parse(offerrem3())),
-                    new AskingResponse.OrderEntry(parse(offerho4()),  parse(offerrem4())),
-                    new AskingResponse.OrderEntry(parse(offerho5()),  parse(offerrem5())),
-                    new AskingResponse.OrderEntry(parse(offerho6()),  parse(offerrem6())),
-                    new AskingResponse.OrderEntry(parse(offerho7()),  parse(offerrem7())),
-                    new AskingResponse.OrderEntry(parse(offerho8()),  parse(offerrem8())),
-                    new AskingResponse.OrderEntry(parse(offerho9()),  parse(offerrem9())),
-                    new AskingResponse.OrderEntry(parse(offerho10()), parse(offerrem10()))
+                    new AskingResponse.OrderEntry(parse(offerho1()),  parse(untOfferrem1())),
+                    new AskingResponse.OrderEntry(parse(offerho2()),  parse(untOfferrem2())),
+                    new AskingResponse.OrderEntry(parse(offerho3()),  parse(untOfferrem3())),
+                    new AskingResponse.OrderEntry(parse(offerho4()),  parse(untOfferrem4())),
+                    new AskingResponse.OrderEntry(parse(offerho5()),  parse(untOfferrem5())),
+                    new AskingResponse.OrderEntry(parse(offerho6()),  parse(untOfferrem6())),
+                    new AskingResponse.OrderEntry(parse(offerho7()),  parse(untOfferrem7())),
+                    new AskingResponse.OrderEntry(parse(offerho8()),  parse(untOfferrem8())),
+                    new AskingResponse.OrderEntry(parse(offerho9()),  parse(untOfferrem9())),
+                    new AskingResponse.OrderEntry(parse(offerho10()), parse(untOfferrem10()))
             );
         }
 
         public List<AskingResponse.OrderEntry> toBids() {
             return List.of(
-                    new AskingResponse.OrderEntry(parse(bidho1()),  parse(bidrem1())),
-                    new AskingResponse.OrderEntry(parse(bidho2()),  parse(bidrem2())),
-                    new AskingResponse.OrderEntry(parse(bidho3()),  parse(bidrem3())),
-                    new AskingResponse.OrderEntry(parse(bidho4()),  parse(bidrem4())),
-                    new AskingResponse.OrderEntry(parse(bidho5()),  parse(bidrem5())),
-                    new AskingResponse.OrderEntry(parse(bidho6()),  parse(bidrem6())),
-                    new AskingResponse.OrderEntry(parse(bidho7()),  parse(bidrem7())),
-                    new AskingResponse.OrderEntry(parse(bidho8()),  parse(bidrem8())),
-                    new AskingResponse.OrderEntry(parse(bidho9()),  parse(bidrem9())),
-                    new AskingResponse.OrderEntry(parse(bidho10()), parse(bidrem10()))
+                    new AskingResponse.OrderEntry(parse(bidho1()),  parse(untBidrem1())),
+                    new AskingResponse.OrderEntry(parse(bidho2()),  parse(untBidrem2())),
+                    new AskingResponse.OrderEntry(parse(bidho3()),  parse(untBidrem3())),
+                    new AskingResponse.OrderEntry(parse(bidho4()),  parse(untBidrem4())),
+                    new AskingResponse.OrderEntry(parse(bidho5()),  parse(untBidrem5())),
+                    new AskingResponse.OrderEntry(parse(bidho6()),  parse(untBidrem6())),
+                    new AskingResponse.OrderEntry(parse(bidho7()),  parse(untBidrem7())),
+                    new AskingResponse.OrderEntry(parse(bidho8()),  parse(untBidrem8())),
+                    new AskingResponse.OrderEntry(parse(bidho9()),  parse(untBidrem9())),
+                    new AskingResponse.OrderEntry(parse(bidho10()), parse(untBidrem10()))
             );
         }
 
