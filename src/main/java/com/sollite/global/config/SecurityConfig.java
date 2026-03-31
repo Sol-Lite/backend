53912f7bf7aae63613b2e4b3054243e6a5686d6d
combe4259 <han97901@gmail.com>
2026-04-01 04:02:30 +0900

    fix(security): actuator/health 엔드포인트 인증 예외 추가

--- src/main/java/com/sollite/global/config/SecurityConfig.java
+++ src/main/java/com/sollite/global/config/SecurityConfig.java
@@ -66,7 +66,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/swagger-resources/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/actuator/health"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
